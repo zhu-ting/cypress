66b3a3105c217473ae6ee0304d23fb14c80605c0
zhu-ting <850832627@qq.com>
2021-01-09 11:00:25 +1100

    make the variable itemText

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -18,16 +18,20 @@ describe('Input form', () => {
 
     context('Form submission', () => {
         it.only('Adds a new todo on submit', () => {
+            const itemText = 'Buy eggs'
             cy.server()
             cy.route('POST', '/api/todos', {
-                name: 'Buy yoghurt',
+                name: itemText,
                 id: 1,
                 isCompleted: false
             })
 
             cy.get('.new-todo')
-              .type('Buy eggs')
+              .type(itemText)
               .type('{enter}')
+            cy.get('.todo-list li')
+              .should('have.length', 1)
+              .and('contain', itemText)
         })
     })
 })
