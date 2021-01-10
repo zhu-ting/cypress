6cedb433a9e752dd38fa35435864cc59a0edd980
zhu-ting <850832627@qq.com>
2021-01-10 20:21:02 +1100

    add todo fixture

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,30 +1,7 @@
-const todos = [
-    {
-        "id": 1,
-        "name": "Buy Milk",
-        "isCompleted": false
-    },
-    {
-        "id": 2,
-        "name": "Buy Eggs",
-        "isCompleted": false
-    },
-    {
-        "id": 3,
-        "name": "Buy Bread",
-        "isCompleted": false
-    },
-    {
-        "id": 4,
-        "name": "Buy Capsicum",
-        "isCompleted": false
-    }
-]
-
 describe('App initialization', () => {
     it.only('Loads todos on page load', () => {
         cy.server()
-        cy.route('GET', '/api/todos', todos)
+        cy.route('GET', '/api/todos', 'fixture:todos')
         cy.visit('/')
 
         cy.get('.todo-list li')
