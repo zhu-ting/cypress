4de19199da5788ec47048e3b20083f3bc8edd1cb
zhu-ting <850832627@qq.com>
2021-01-17 10:27:41 +1100

    Filters to completed todos

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -19,12 +19,19 @@ describe('Footer', () => {
             cy.get('.todo-count')
               .should('contain', '3 todos left')
         })
-        it.only('Filters to active todos', () => {
+        it('Filters to active todos', () => {
             cy.contains('Active')
               .click()
 
             cy.get('.todo-list li')
               .should('have.length', 3)
+        })
+        it.only('Filters to completed todos', () => {
+            cy.contains('Completed')
+              .click()
+
+            cy.get('.todo-list li')
+              .should('have.length', 1)
         })
     })
 
